9326b1694febbf0840af72ed2a067ab6ed698f6e
Vitor Lima <vitor@ferreiradelima.com>
2016-06-29 21:57:53 -0300

    Added utils to index.js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.Account  = require('./account/');
 exports.Analysis = require('./analysis/');
 exports.Device   = require('./device/');
 exports.Services = require('./services/');
+exports.Utils    = require('./utils/');
