290983d77834293018c39bfee7469145b43bbae5
Vitor Lima <vitor@ferreiradelima.com>
2016-07-27 18:45:42 -0300

    Updated tago realtime URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = {
     'api_url'     : process.env.TAGO_API || 'https://api.tago.io',
-    'realtime_url': process.env.TAGO_REALTIME || 'wss://api.tago.io:86',
+    'realtime_url': process.env.TAGO_REALTIME || 'wss://realtime.tago.io',
 };
