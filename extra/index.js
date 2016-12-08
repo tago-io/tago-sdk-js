54761cb9184b270d7648b943c798f810ed378896
Vitor Lima <vitor@ferreiradelima.com>
2016-12-08 14:25:26 -0200

    Added Extra layer

---
+++ extra/index.js
@@ -0,0 +1,22 @@
+'use strict';
+const services = {
+    weather: require('./weather'),
+    currency: require('./currency'),
+    geocoding: require('./geocoding'),
+    distance: require('./distance')
+};
+
+class Extra {
+    constructor(service, api_key) {
+        if (!service) { throw 'Missing parameter service'; }
+        else if (!api_key) { throw 'Missing parameter api_key'; }
+
+        if (!services[service]) {
+            throw `Can\'t find service ${service}`;
+        }
+
+        return new services[service](api_key);
+    }
+}
+
+module.exports = Extra;
