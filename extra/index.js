b89ad6fdf02f27dedb1c973ee8f8ba3d14c2b419
Vitor Lima <vitor@ferreiradelima.com>
2017-09-05 10:41:08 -0300

    Fixed extra and added notifications

--- extra/index.js
+++ extra/index.js
@@ -1,39 +1,21 @@
-'use strict';
-const request         = require('../comum/tago_request.js');
-const config          = require('../config.js');
-const default_headers = require('../comum/default_headers.js');
+const services = {
+    weather: require('./weather'),
+    currency: require('./currency'),
+    geocoding: require('./geocoding'),
+    distance: require('./distance')
+};
 
-/** Class for the device and data */
-class Middleware {
-    /** Device
-     * @param  {String} Middleware Token
-     * @return {Object} Middleware Object
-     */
-    constructor(token, details) {
-        this.token   = token;
-        this.default_options = {
-            'json': true,
-            'headers': default_headers(this)
-        };
+class Extra {
+    constructor(service, api_key) {
+        if (!service) { throw 'Missing parameter service'; }
+        else if (!api_key) { throw 'Missing parameter api_key'; }
 
-        if (details) {
-            this.default_options.qs = {'details': true};
+        if (!services[service]) {
+            throw `Can\'t find service ${service}`;
         }
-    }
 
-    /** 
-     * Get token of the device by its serial number.
-     * Return nothing if the serie number is not associeted with middleware
-     * @param  {STRING} serial_number serial number of the device
-     * @return {Promise}
-     */
-    getBySerieNum(serial_number) {
-        let url    = `${config.api_url}/middleware/get_by_serienum/${serial_number}`;
-        let method = 'GET';
-
-        let options = Object.assign({}, this.default_options, {url, method});
-        return request(options);
+        return new services[service](api_key);
     }
 }
 
-module.exports = Middleware;
+module.exports = Extra;
---
+++ services/notification.js
@@ -0,0 +1,35 @@
+'use strict';
+const request         = require('../comum/tago_request.js');
+const config          = require('../config.js');
+const default_headers = require('../comum/default_headers.js');
+
+class Notification {
+    constructor(analysis_token) {
+        this.token = analysis_token;
+        this.default_options = {
+            'json':    true,
+            'headers': default_headers(this)
+        };
+    }
+
+    /** Send Notification
+     * You can add ref_id from a bucket or dashboard, if it is valid it will show up a button Go To Dashboard
+     * Any account with share of the dashboard/bucket will receive too.
+     * @param  {string} topic - topic of the message
+     * @param  {string} message - Message scope
+     * @param  {string} [ref_id] - Dashboard/Bucket ID for "Go To" button.
+     * @return {Promise}
+     */
+    send(title, message, ref_id) {
+        let url    = `${config.api_url}/analysis/services/notification/send`;
+        let method = 'POST';
+        let data = { title, message, ref_id };
+
+        let options = Object.assign({}, this.default_options, {url, method, data});
+        return request(options);
+    }
+
+
+}
+
+module.exports = Notification;
