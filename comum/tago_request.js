3de4671cdf0fbe9f8a512694f75af8f0643ad600
Vitor Lima <vitor@ferreiradelima.com>
2017-09-12 16:43:08 -0300

    Added timeout handler

--- comum/tago_request.js
+++ comum/tago_request.js
@@ -1,26 +1,49 @@
 'use strict';
 const axios = require('axios');
+const co    = require('co');
+const config = require('../config');
+
+function resultHandler(request_options, result) {
+    if (!result.data) {
+        throw result.statusText;
+    }
+    else if (request_options.url.indexOf('/data/export') !== -1) {
+        return result.data;
+    }
+    else if (!result.data.status) {
+        throw result.data.message || result;
+    }
+    return result.data.result;
+}
+
+function errorHandler(error) {
+    if (!error.response.data) {
+        throw error;
+    }
+    const { message, status, result } = error.response.data;
+    if (message.includes(['Timeout']) || message.includes(['SequelizeDatabaseError'])) {
+        return false;
+    }
+    else if (!status) {
+        throw message || error;
+    }
+    throw result;
+}
+
+const waitTime = () => new Promise(resolve => setTimeout(() => resolve(), 1000));
 
 module.exports = function tago_request(request_options) {
-    request_options.timeout = 60000;
-    return axios(request_options).then(result => {
-        if (!result.data) {
-            throw result.statusText;
+    return co(function* _() {
+        request_options.timeout = 60000;
+        let result;
+        const _resultHandler = resultHandler.bind(null, request_options);
+        for (let i = 1; i <= config.request_attempts; i+=1) {
+            result = yield axios(request_options).then(_resultHandler, errorHandler);
+            if (result) continue;
+            
+            yield waitTime();
         }
-        else if (request_options.url.indexOf('/data/export') !== -1) {
-            return result.data;
-        }
-        else if (!result.data.status) {
-            throw result.data.message || result;
-        }
-        return result.data.result;
-    }).catch((error) => {
-        if (!error.response.data) {
-            throw error;
-        }
-        else if (!error.response.data.status) {
-            throw error.response.data.message || error;
-        }
-        return error.response.data.result;
+        if (!result) result = 'SDK: Request timed out';
+        return result;
     });
 };
--- config.js
+++ config.js
@@ -2,4 +2,5 @@
 module.exports = {
     'api_url'     : process.env.TAGO_API || 'https://api.tago.io',
     'realtime_url': process.env.TAGO_REALTIME || 'wss://realtime.tago.io',
+    'request_attempts': process.env.REQUEST_ATTEMPTS || 3,
 };
