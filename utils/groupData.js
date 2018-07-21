5ee20a46d7071d314f988c907564ac64da9c224b
rcanoff <rcanoff@gmail.com>
2018-07-20 21:59:13 -0300

    Created the util to group data by series

---
+++ utils/groupData.js
@@ -0,0 +1,47 @@
+
+const device = require('../Device');
+
+/**
+ * Group data by series or time
+ * @param  {Array} variables
+ * @param  {String} groupBy: time or serie
+ * @param  {boolean} valueOnly: defines if the object is variable value only or the full object
+ * @return {Array} returns the array grouped by time or series
+ */
+function groupData(variables, groupBy = 'serie', valueOnly = false) {
+  if (valueOnly) {
+    return Object.values(variables.reduce((pre, cur) => {
+      (pre[cur[groupBy]] = {
+        ...pre[cur[groupBy]],
+        ...{
+          ...{
+            [cur.variable]: cur.value,
+            origin: cur.origin,
+            ...(cur[groupBy] ? { [groupBy]: cur[groupBy] } : {}),
+          },
+        },
+      });
+      return pre;
+    }, {}));
+  }
+
+  return Object.values(variables.reduce((pre, cur) => {
+    (pre[cur[groupBy]] = {
+      ...pre[cur[groupBy]],
+      ...{
+        ...{
+          [cur.variable]: {
+            ...{ value: cur.value, id: cur.id, time: cur.time },
+            ...(cur.metadata ? { metadata: cur.metadata } : {}),
+            ...(cur.location ? { location: cur.location } : {}),
+          },
+          origin: cur.origin,
+          ...(cur.serie ? { serie: cur.serie } : {}),
+        },
+      },
+    });
+    return pre;
+  }, {}));
+}
+
+module.exports = groupData;
