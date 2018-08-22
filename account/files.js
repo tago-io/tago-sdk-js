bc86f805a01626e68aa12e5e327e559d8e57d6a5
Felipe Lima <felipe@ferreiradelima.com>
2018-08-22 19:05:06 -0300

    added files functions

--- account/files.js
+++ account/files.js
@@ -60,6 +60,112 @@ class Files {
 
     return request(options);
   }
+
+  /**
+   * Move/Rename Files
+   * @param {Array.<{from:String, to:String}>} arrayOfFileObjects
+   *
+   * Exemple:
+   *  [ { from: '/myfiles/myOldName.ext', to: '/myfiles/newFolder/andNewName.ext' }]
+   */
+  move(arrayOfFileObjects) {
+    let url = `${config.api_url}/files`;
+    let method = 'PUT';
+
+    let options = Object.assign({}, this.default_options, {
+      url,
+      method,
+      paramsSerializer,
+      data: arrayOfFileObjects
+    });
+
+    return request(options);
+  }
+
+  /**
+   * Delete Folder or Files
+   * @param {String} path Path of whole folder or single file
+   *
+   */
+  delete(path) {
+    let url = `${config.api_url}/files`;
+    let method = 'DELETE';
+
+    let options = Object.assign({}, this.default_options, {
+      url,
+      method,
+      paramsSerializer,
+      params: {
+        path,
+      },
+    });
+
+    return request(options);
+  }
+
+  /**
+   * Check if file is private or public
+   * @param {String} file Path of file
+   *
+   */
+  checkPermission(file) {
+    let url = `${config.api_url}/files/permission`;
+    let method = 'GET';
+
+    let options = Object.assign({}, this.default_options, {
+      url,
+      method,
+      paramsSerializer,
+      params: {
+        file,
+      },
+    });
+
+    return request(options);
+  }
+
+  /**
+   * Make the file private or public
+   * @param {Array.<{file:String, public:Boolean}>} arrayOfFileObjects
+   *
+   * Exemple:
+   *  [ { file: '/myfiles/myFile.txt', public: true }]
+   */
+  changePermission(arrayOfFileObjects) {
+    let url = `${config.api_url}/files/permission`;
+    let method = 'PUT';
+
+    let options = Object.assign({}, this.default_options, {
+      url,
+      method,
+      paramsSerializer,
+      data: arrayOfFileObjects
+    });
+
+    return request(options);
+  }
+
+  /**
+   * Get file with authenticate token for privates files
+   * @param {String} url Full TagoIO File url
+   * Get a file with authenticate token valid for 120 seconds.
+   */
+  getFileURLSigned(url) {
+    const method = 'GET';
+    if (String(url).indexOf('.tago.io/file/') === -1) {
+      return Promise.reject(`${url} is not a TagoIO files url`);
+    }
+
+    let options = Object.assign({}, this.default_options, {
+      url,
+      method,
+      params: {
+        noRedirect: true,
+      }
+    });
+
+    return request(options);
+  }
 }
 
 module.exports = Files;
