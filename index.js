f1893bc49eb4a6c5146ebd33dde6bc5f50b6eb80
Vitor Lima <vitor@ferreiradelima.com>
2019-07-16 03:10:29 -0300

    Added new libraries to index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
-exports.Account    = require('./account/');
-exports.Analysis   = require('./analysis/');
-exports.Device     = require('./device/');
-exports.Middleware = require('./middleware/');
-exports.Services   = require('./services/');
-exports.Utils      = require('./utils/');
-exports.RunUser    = require('./run_user/');
+exports.Account       = require('./account/');
+exports.Analysis      = require('./analysis/');
+exports.Device        = require('./device/');
+exports.Middleware    = require('./middleware/');
+exports.Services      = require('./services/');
+exports.Utils         = require('./utils/');
+exports.RunUser       = require('./run_user/');
+exports.Connector     = require('./connector/');
+exports.Authorization = require('./authorization/');
